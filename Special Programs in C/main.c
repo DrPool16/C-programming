a547d087e738aeaf2bebe228e02660a7e0f8f32a
DrPool16 <elmesdepool@gmail.com>
2024-02-01 23:59:23 -0500

    Special programs Prime number/Add without + Operator

--- Special Programs in C/main.c
+++ Special Programs in C/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+
+    *sqrt means square root
+    *available in math.h library
+    Syntax: double sqrt(double x)
+    - sqrt(2) = 1.414
+
+    *ceil function returns a smallest integer greater than or equal to x
+    *available in math.h library
+    Syntax: double ceil(double x)
+    - ceil(1.414 = 2)
+
+
+*/
 
 int main()
 {
@@ -11,9 +25,17 @@ int main()
     int pal;
     int arms;
     int stnum;
+    int prime=0;
+    int num1, num2;
     switch(chose){
     case 1:
-        /*Pyramid of Stars*/
+        /*Pyramid of Stars
+                *
+               ***
+              *****
+             *******
+            *********
+                           */
         printf("How many rows do you want in your pyramid?\n");
         scanf("%d", &rows);
 
@@ -78,7 +100,7 @@ int main()
         /*Strong Number*/
         /*145 = 1! + 4! + 5! = 1 + 24 + 120 = 145
            3! = n*(n-1)*(n-2) */
-        printf("Write a number:\n");
+        printf("Write a number - Strong number:\n");
         scanf("%d", &stnum);
         int aux_str, last_n = 0, total=1, sumatory=0;
 
@@ -99,19 +121,47 @@ int main()
             printf("The number %d is a Strong number\n",stnum);
         }else{
             printf("The number %d is not a Strong number\n",stnum);
-        };
+        }
 
+        break;
+    case 5:
+        /*Prime number
+        Prime numbers are numbers greater than 1 that only have two factors,
+        1 and the number itself. This means that a prime number is only divisible
+        by 1 and itself. If you divide a prime number by a number other than
+        1 and itself, you will get a non-zero remainder. Ex:  13--> 1 and 13 only, but 6 has 1, 2, 3 and 6 divisors */
+        printf("Write a number - Prime Number:\n");
+        scanf("%d", &prime);
+
+        int conta = 1,remain = 0, co = 0;
+        while(conta <= prime){
+            remain = prime%conta;
+            if (remain == 0){
+                co++;
+            }
+            conta++;
+        }
+        if(co == 2){
+            printf("The number %d is a Prime number\n",prime);
+        }else{
+            printf("The number %d is not a Prime number\n",prime);
+        }
+    case 6:
+        /*Addition without using '+' operator*/
+        printf("Write two number - Add two numbers without using '+' operator:\n");
+        scanf("%d %d", &num1, &num2);
+
+        int suma = num1;
+        for (int m=0; m<num2; m++) suma++;
+        for (int n=num2; n<0;n++) suma--;
+        printf("The sum of %d and %d is %d", num1, num2, suma);
         break;
     }
     return 0;
 }
 /*
 
-    *
-   ***
-  *****
- *******
-*********
+
 
 */
 
