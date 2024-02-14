4861bfc5e6a957246d2498aeb8d118f3bf0d04c1
DrPool16 <elmesdepool@gmail.com>
2024-02-13 22:42:46 -0500

    Pointers as an Arrays

--- Pointers/main.c
+++ Pointers/main.c
@@ -96,6 +96,53 @@ IMPORTNAT POINTS
             - Performing arithmetic on pointers which are not pointing to array element leads to undefined behavior.
             - If two pointers are pointing to different arrays then performing subtraction between them leads to undefined behavior.
 
+    Increment & Decrement
+        Post
+        int *p = &a[0];
+        *(p++);
+        Pre
+        *(++p);
+
+    Comparing Pointers
+        * Use relational operators(<,>,<=,>=) and equality operators (==, !=) to compare pointers.
+        * Only possible when both pointers point to same array.
+        * Output depends upon the relative positions of both the pointers.
+
+    Using Array Name as a Pointer
+        * Name of an array can be used as a pointer to the first element of an array.
+
+        int a[5];
+        *a =10;             // *a = 10; === *(1000) = 10;
+        printf("%d",a[0]);  // 10
+        *(a+1) = 20;        // *(a+1) = 20; === *(1000 + 1x4(bytes)); === *(1004) = 20;
+        printf("%d",a[1]);  // 20
+
+        *(a + i) = a[i];
+        Be carefully
+           a++; ERROR // We are trying to assign address 1004 to array name 'a';
+                         Recall that name of the array indicates the base address of the array i.e 1000.
+                         We cannot change this.
+
+        There is no problem in the code below
+            a+1;      // Here, we are not trying to assign some new address to 'a'.
+                         We are simply accessing the address of the second element of the array.
+                         But is not a good practice.
+        Alternative
+            int *p =a;
+            *(++p);
+
+        Conclusion is that we cannot assign a new address to the name of the array,
+        but we can use a pointer which can be representative of the array, and then we can
+        perform any arithmetic that we can perform.
+
+    Using pointers with two dimensional arrays
+        Difference between row major and column major order
+        - Raw major order: Elements are stored row by row.
+        - Column major order: Elements are stored column by column.
+        Important Fact
+        - C stores multidimensional arrays in row major order.
+
+
 
 
 */
@@ -116,6 +163,14 @@ void minMax(int arr[], int len, int *min, int *max){
 int *findMid(int b[], int n){
     return &b[n/2];   // We are returning the address of the contain of the mid element.
 }
+
+/*Passing Array as an Argument to a Function*/
+int add(int b[], int len){       // We can also write int *b;
+    int sum = 0, i;
+    for(i=0;i<len;i++) sum += b[i];
+    return sum;
+}
+#define N 5
 
 int main()
 {
@@ -144,6 +199,73 @@ int main()
     int l = 10;
     int *k = &l;
     printf("The address of variable l is %p\n",p);
-    return 0;
 
+    /*Increment & Decrement*/
+    int c[] = {5,16,7,89,45,32,23,10};
+    int *po = &c[0]; // *po = 1000;
+    /*Post Incre..*/
+    printf("%d ",*(po++)); // 5, *po = 1000; after execution: *po = 1004;
+    printf("%d ",*po); // 16, *po = 1004;
+    /*Pre Incre..*/
+    printf("%d ",*(++po)); // 7, *po = 1004; before execution:*po = 1008; *(++p) === *(1008)
+
+    /*Pre & Post Decrement*/
+    printf("%d ",*(--p)); // 16
+    printf("%d\n",*(p--)); // 16
+
+    /*Comparing Pointers*/
+    po = &c[3];
+    int *qo =&c[5];
+    printf("%d ",po <= qo); // 1
+    printf("%d ",po >= qo); // 0
+    qo =& c[3];
+    printf("%d\n",po == qo); // 1
+
+    int *p1 = &c[1], *q1 = &c[5];
+
+    printf("%d",*(p1+3));     // *c[1 + 3] = 45;
+    printf("%d",*(q1-3));     // *c[5 - 3] = 7;
+    printf("%d", q1 - p1);    // &c[5] - &c[1] === 5 - 1 = 4;
+    printf("%d", p1 < q1);    // 1 < 5 = 1;
+    printf("%d\n",*p1 < *q1);   // 16 < 32 = 1;
+
+
+    /*Sum of elements of Array*/
+    int s[] = {11,22,36,5,2};
+    int sum=0, *pt;
+    pt = &s[0];
+    for(pt=&s[0]; pt <= &s[4]; pt++){
+        sum += *pt;
+    }
+    /*
+    // Also works //
+    int lent = sizeof(s)/sizeof(s[0]);
+    for(int i=0; i<lent;i++){
+        sum += *(pt++);
+    }
+    // Also works //
+    for(pt=s; pt<=s+4;pt++){
+        sum+= *pt;
+    }
+    */
+    printf("The sum of elements of Array c: %d\n",sum);
+
+    /*Reversing a Series of Numbers using Pointers*/
+    int d[N], *pr;
+    printf("Enter %d elements in the array: ", N);
+    for(pr=d;pr<= d+(N-1);pr++) scanf("%d",pr);         // pr = 1000; pr <= 1000+(5-1)x4(bytes); pr++; scanf --> pr = &d[0]; In the first statement
+
+    printf("Elements in reverse order: \n");
+    for(pr=d+(N-1); pr >= d;pr--)  printf("%d ", *pr);
+
+    /*Passing Array as an Argument to a Function*/
+    int lenn = sizeof(a)/sizeof(a[0]);
+    printf("\n%d",add(a,lenn)); // We are not passing the whole array. We are just passing the base address of the array.
+                                // An array name is always treated as a pointer.
+
+    /*Using pointers with two dimensional arrays*/
+    //for(p=a&[0][0]; p <= &a[row-1][col-1]; p++) printf("%d",*p); Output: 1 2 3 4
+
+
+    return 0;
 }
